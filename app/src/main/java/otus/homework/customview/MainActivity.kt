5260932a70430fa2ffaee9be7ca956e19143dfed
User <asdfs@sfsdfa.sfd>
2024-06-18 15:30:42 +0300

    + Gson

--- app/src/main/java/otus/homework/customview/MainActivity.kt
+++ app/src/main/java/otus/homework/customview/MainActivity.kt
@@ -2,10 +2,24 @@ package otus.homework.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RawRes
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import otus.homework.data.Payment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data: List<Payment> = readRawJson(R.raw.payload)
+
+    }
+
+    private inline fun <reified T> readRawJson(@RawRes rawResId: Int): T {
+        val gson = Gson()
+        resources.openRawResource(rawResId).bufferedReader().use {
+            return gson.fromJson<T>(it, object: TypeToken<T>() {}.type)
+        }
     }
 }
