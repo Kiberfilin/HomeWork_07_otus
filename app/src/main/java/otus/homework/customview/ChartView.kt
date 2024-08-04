934b6cf4c7db56a91a802e23d4ea6ae2be9f314d
Kiberfilin <kiberfilin@gmail.com>
2024-08-04 06:50:02 +0300

    рисуем оси и сетку 1

--- app/src/main/java/otus/homework/customview/ChartView.kt
+++ app/src/main/java/otus/homework/customview/ChartView.kt
@@ -2,8 +2,10 @@ package otus.homework.customview
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
@@ -11,7 +13,6 @@ import otus.homework.data.Payment
 import java.math.BigDecimal
 import java.util.Calendar
 import java.util.Date
-import java.util.Random
 
 class ChartView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -19,21 +20,35 @@ class ChartView @JvmOverloads constructor(
     companion object {
         private const val DEFAULT_STEP: Int = 50
         private const val DAY_IN_MILLS: Int = 1000 * 60 * 60 * 24
-        private const val OFFSET_START: Int = 20
-        private const val OFFSET_TOP: Int = 20
+        private const val OFFSET_START: Int = 50
+        private const val OFFSET_TOP: Int = 50
+        private const val OFFSET_BOTTOM: Int = 50
+        private const val X_TAIL: Int = 50
+        private const val MAX_SPENT_AMOUNT_IN_DAY_Y_PERSENTAGE: Float = 0.9F
     }
 
     private val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ChartView)
     private val step = typedArray.getDimension(R.styleable.ChartView_stepInDp, DEFAULT_STEP.dp)
+    private val axisPaint = Paint().apply {
+        style = Paint.Style.STROKE
+        isAntiAlias = true
+        color = Color.DKGRAY
+        alpha = 200
+        strokeWidth = 5f
+    }
+
     private val greedPaint = Paint().apply {
         style = Paint.Style.STROKE
         isAntiAlias = true
         color = Color.DKGRAY
         alpha = 200
-        strokeWidth = 3f
+        strokeWidth = 2f
     }
     private val paymentList: ArrayList<PayDay> = ArrayList()
     private var days: Int = 0
+    private val axisPath = Path()
+    private val greedPath = Path()
+    private var maxSpentInOneDay: Float = 0F
 
     @ColorInt
     private var categoryColor: Int = 0
@@ -46,6 +61,7 @@ class ChartView @JvmOverloads constructor(
             clear()
             addAll(calculatePaymentData(categoryPayments))
         }
+        maxSpentInOneDay = paymentList.maxBy { it.amount }.amount.toFloat()
         requestLayout()
     }
 
@@ -80,6 +96,40 @@ class ChartView @JvmOverloads constructor(
         val minTime = data.minBy { it.time }.time
         val maxTime = data.maxBy { it.time }.time
         return ((maxTime - minTime) / DAY_IN_MILLS).toInt() + 1
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val xStart: Float = OFFSET_START.toFloat()
+        val yStart: Float = OFFSET_TOP.toFloat()
+        val xFinish: Float = OFFSET_START.toFloat() + step * days + X_TAIL
+        val yFinish: Float = height - OFFSET_TOP.toFloat() - OFFSET_BOTTOM.toFloat()
+        greedPath.apply {
+            reset()
+            for (day in 1..days) {
+                // рисуем вертикальные линии сетки
+                val xDay: Float = xStart + day * step
+                moveTo(xDay, yStart)
+                lineTo(xDay, yFinish)
+            }
+        }
+        axisPath.apply {
+            reset()
+            // ось y: деньги
+            moveTo(xStart, yStart)
+            lineTo(xStart, yFinish)
+            // ось x: дни
+            moveTo(xStart, yFinish)
+            lineTo(xFinish, yFinish)
+        }
+
+        canvas.apply {
+            //рисуем оси и сетку, если есть количество дней уже известно
+            if (days != 0) {
+                drawPath(axisPath, axisPaint)
+                drawPath(greedPath, greedPaint)
+            }
+        }
     }
 }
 
